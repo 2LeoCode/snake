9c707bf9886fa7f7752471df43a7fe2334c195f5
2LeoCode <leo.13suardi@outlook.com>
2020-05-24 19:31:08 +0200

    v1.0-norme

--- inc/snake.h
+++ inc/snake.h
@@ -6,7 +6,7 @@
 /*   By: lsuardi <lsuardi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/23 00:36:26 by lsuardi           #+#    #+#             */
-/*   Updated: 2020/05/24 19:17:29 by lsuardi          ###   ########.fr       */
+/*   Updated: 2020/05/24 19:30:48 by lsuardi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,29 +59,31 @@ typedef struct		s_list
 ** main
 */
 
-int			get_elems(t_list **snake, t_coords *bonus, t_head face, double *speed);
-t_coords	generate_bonus(void);
-void		display_elems(t_list *snake, t_coords bonus, WINDOW *game);
-int			get_direction(t_head prev);
-int			get_next_pos(t_list **snake, t_coords bonus, t_head new_head);
-t_bool		check_input(int input);
-void		get_next_head(t_list **snake);
-void		add_member(t_list **snake);
-int			show_leaderboard(void);
-int			choose_option(void);
-int			check_pos(t_list *snake_head);
-int			pause_menu(void);
-void		init_game(t_list *snake, WINDOW *game);
+int					get_elems(t_list **snake, t_coords *bonus,
+t_head face, double *speed);
+t_coords			generate_bonus(void);
+void				display_elems(t_list *snake, t_coords bonus, WINDOW *game);
+int					get_direction(t_head prev);
+int					get_next_pos(t_list **snake, t_coords bonus,
+t_head new_head);
+t_bool				check_input(int input);
+void				get_next_head(t_list **snake);
+void				add_member(t_list **snake);
+int					show_leaderboard(void);
+int					choose_option(void);
+int					check_pos(t_list *snake_head);
+int					pause_menu(void);
+void				init_game(t_list *snake, WINDOW *game);
 
 /*
 ** utils
 */
 
-t_list		*ft_lstnew(int x, int y, t_head head);
-int			ft_atoi(const char *str);
-void		ft_fputs(int fd, char *s);
-t_bool		ft_kbhit(void);
-t_list		*ft_lstlast(t_list *lst);
-void		ft_lstclear(t_list **lst);
-void		ft_wait(double seconds);
+t_list				*ft_lstnew(int x, int y, t_head head);
+int					ft_atoi(const char *str);
+void				ft_fputs(int fd, char *s);
+t_bool				ft_kbhit(void);
+t_list				*ft_lstlast(t_list *lst);
+void				ft_lstclear(t_list **lst);
+void				ft_wait(double seconds);
 #endif
